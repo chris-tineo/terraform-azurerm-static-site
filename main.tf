a54134a5938eb29e80b87f490d453eaefb74a479
chris-tineo <52720074+chris-tineo@users.noreply.github.com>
2022-04-06 03:48:30 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,6 @@ resource "azurerm_static_site" "staticsite" {
   name                = var.rsrc_name
   resource_group_name = var.rsrc_rg
   location            = var.rsrc_location
+  sku_tier            = var.rsrc_skugeneral
+  sku_size            = var.rsrc_skugeneral
 }
